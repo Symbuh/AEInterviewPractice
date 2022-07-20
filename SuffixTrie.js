d69b044556e563724ef97f4344d535304e4a05bc
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-07-19 17:02:36 -0700

    Firt commit of AE interview practice. Implementation of rudementary suffix trie

---
+++ SuffixTrie.js
@@ -0,0 +1,54 @@
+class SuffixTrie {
+  constructor(string) {
+    this.root = {};
+    this.endSymbol = '*';
+    this.populateSuffixTrieFrom(string);
+  }
+
+  populateSuffixTrieFrom(string) {
+    let currentNode;
+
+    for (let i = 0; i < string.length; i++) {
+      if (!this.root[string[i]]) {
+        this.root[string[i]] = {};
+      }
+      currentNode = this.root[string[i]];
+
+      // Populating subnodes
+      for (let j = i + 1; j < string.length; j++) {
+        if (!currentNode[string[j]]) {
+          currentNode[string[j]] = {};
+        }
+        currentNode = currentNode[string[j]];
+      }
+      // CurrentNode is a complete word at this point:
+      currentNode['*'] = true;
+    }
+    console.log(this.root);
+  }
+
+  contains(string) {
+    if (!this.root[string[0]]) {
+      return false;
+    }
+
+    let currentNode = this.root[string[0]];
+
+    for (let i = 1; i < string.length; i++) {
+      if (!currentNode[string[i]]) {
+        return false;
+      }
+
+      currentNode = currentNode[string[i]];
+    }
+
+    if (currentNode['*'] === true) {
+      return true;
+    }
+
+    return false;
+  }
+}
+
+// Do not edit the line below.
+exports.SuffixTrie = SuffixTrie;
