332739e22f81e1e4f9c1f3253a95f59cfbc694b8
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-08-05 23:23:12 -0700

    Moar optional chaining

---
+++ ValidateBST.js
@@ -0,0 +1,24 @@
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function validateBst(tree) {
+  let validateBSTHelper = (tree, minValue, maxValue) => {
+    if (!tree) {
+      return true;
+    }
+
+    if (tree?.value < minValue || tree?.value >= maxValue) {
+      return false;
+    }
+
+    let leftIsValid = validateBSTHelper(tree.left, minValue, tree.value);
+    return leftIsValid && validateBSTHelper(tree.right, tree.value, maxValue);
+  };
+
+  return validateBSTHelper(tree, -100000, 100000);
+}
