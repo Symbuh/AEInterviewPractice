6ebc0b93ce1aaa6a65de26d0ead64f1e5234ed60
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2022-08-18 20:09:49 -0700

    Added problem statement for Maximum Subset Sum

--- maximumSumNonAdjacent.js
+++ maximumSumNonAdjacent.js
@@ -1,3 +1,10 @@
+// Write a function that takes in an array of positive integers and returns the maximum sum of non-adjacent elements in the array.
+
+// if the input array is empty, the function should return 0
+
+// Input: [75, 105, 120, 75, 90, 135]
+// output: 330 (75 + 120 + 135)
+
 function maxSubsetSumNoAdjacent(array) {
   let sum2Back = array[0],
     sum1Back = array[1] > sum2Back ? array[1] : sum2Back;
