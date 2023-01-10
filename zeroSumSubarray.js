1d8b3ff85bc7105099c3b03167425d9a78d86855
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2023-01-10 11:28:39 -0800

    Added some comments to zeroSumSubarray

--- zeroSumSubarray.js
+++ zeroSumSubarray.js
@@ -1,3 +1,5 @@
+// The idea here is to store the sum of the array from 0 to N and the index of N in an object
+// If we ever get a duplicate sum we know there is an existing subarray which adds to 0.
 function zeroSumSubarray(nums) {
   let leftSideSum = {},
     firstPassTotal = 0;
